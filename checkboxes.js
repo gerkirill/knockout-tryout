4e72752fd76266bfc38ebf41f3e4f53853e0f8ba
gerkirill <ger.kirill@gmail.com>
2013-06-28 09:56:34 +0300

    added comments to js, corrected one mistake

--- checkboxes.js
+++ checkboxes.js
@@ -1,10 +1,16 @@
 checkboxes = function() {
     var self = this;
+
+    /** contains checkbox observables, key format is cb_<checkbox value> */
     self.checkboxes = {};
+
+    /** create observables for checkboxes */
     $('.data-checkboxes .option').each(function(){
         var checkbox = this;
         self.checkboxes['cb_'+checkbox.value] = ko.observable(checkbox.checked);
     });
+
+    /** calculated property - number of checked checkboxes */
     self.checkedCount = ko.computed(function(){
         var cnt = 0;
         for (var k in self.checkboxes) {
@@ -12,6 +18,8 @@ checkboxes = function() {
         }
         return cnt;
     });
+
+    /** calculated property - number of unchecked checkboxes */
     self.uncheckedCount = ko.computed(function(){
         var cnt = 0;
         for (var k in self.checkboxes) {
@@ -19,16 +27,20 @@ checkboxes = function() {
         }
         return cnt;
     });
+
+    /** calculated property, boolean. True if there are both checked and unchecked checkboxes. */
     self.heterogenous = ko.computed(function(){
         return self.uncheckedCount() > 0 && self.checkedCount() > 0;
     })
+
+    /** calcualted writable property, changes "check all" checkbox status and reacts on its status change */
     self.switcherChecked = ko.computed({
         read: function(){
             return self.checkedCount() > 0;
         },
         write: function(value) {
             for (var k in self.checkboxes) {
-                if (self.checkboxes[k](value));
+                self.checkboxes[k](value)
             }
         }
     });
